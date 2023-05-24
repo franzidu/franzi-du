499c0bedda8d366ed5e6e8f37758aea3878b2849
franziska.dudichum <franzi-du@web.de>
2023-05-24 22:25:03 +0200

    moin

---
+++ franzi-du/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import "./Navbar.modules.scss";
+
+function Navbar() {
+  return (
+    <nav>
+      <div>
+        <p>hello</p>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
